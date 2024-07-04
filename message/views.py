8bb11b6139bd78e6c81920999b898aabd9f02876
t <11230013@wfsh.tp.edu.tw>
2024-07-04 13:12:02 +0800

    留言列表初始

--- message/views.py
+++ message/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Message     #models.py  => Message
 
 # Create your views here.
+class MessageList(ListView):
+    model = Message
+
+    #規則 : 應用程式 / 資料模型_list.html
+    #message / message_list.html
+
+    #資料模型_list
